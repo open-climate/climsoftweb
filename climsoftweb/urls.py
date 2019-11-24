5b73e082131b5bd6b3790d1864c52f53442736d5
isedwards <ian@myacorn.com>
2019-11-24 08:33:58 +0000

    Fix import

--- climsoftweb/urls.py
+++ climsoftweb/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import main
+from main import views
 
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('/', main.views.menu, name='mainmenu'),
+    path('/', views.menu, name='mainmenu'),
 ]
