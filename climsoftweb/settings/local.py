fffa4d23e6e98c9f7fbc6a32bd54d67a64aa497e
isedwards <ian@myacorn.com>
2020-02-09 15:53:18 +0000

    Update django setting to use production settings by default

---
+++ climsoftweb/settings/local.py
@@ -0,0 +1,23 @@
+from climsoftweb.settings.base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['192.168.1.161']
+
+
+# INSTALLED_APPS += ('debug_toolbar',)
+#
+# MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+#
+# # The Django Debug Toolbar will only be shown to these client IPs.
+# INTERNAL_IPS = (
+#     '127.0.0.1',
+#     '192.168.33.1',
+# )
+#
+# DEBUG_TOOLBAR_CONFIG = {
+#     'INTERCEPT_REDIRECTS': False,
+#     'SHOW_TEMPLATE_CONTEXT': True,
+#     'HIDE_DJANGO_SQL': False,
+# }
---
+++ climsoftweb/settings/production.py
@@ -0,0 +1,8 @@
+from climsoftweb.settings.base import *
+
+
+# TODO: Set security key from environment variable
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['dev3.climsoft.org']
